dd5f92d601eaefcbac8b78ba7b089ebb1e6cf637
zouheir owaida <zoher.owida1@gmail.com>
2020-06-30 22:10:29 +0300

    - clean code

---
+++ database/migrations/2020_06_30_152755_create_questionnaires_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionnairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('clientId');
+            $table->integer('productId');
+            $table->string('price');
+            $table->string('discount');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+}
